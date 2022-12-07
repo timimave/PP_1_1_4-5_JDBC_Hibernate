95c23de54b07743c35d415a8070b37da378f66b5
TIm IM <timimave@gmail.com>
2022-12-07 14:18:41 +0500

    rdyTeat

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,10 +7,10 @@ public class Main {
     public static void main(String[] args) {
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
-        userService.saveUser("Ivan", "Ivanov", (byte) 23);
-        userService.saveUser("Petr", "Petrov", (byte) 32);
-        userService.saveUser("Sergey", "Smirnov", (byte) 20);
-        userService.saveUser("Andrey", "Khalezin", (byte) 33);
+        userService.saveUser("Mike", "Mayer", (byte) 23);
+        userService.saveUser("Bob", "Dylan", (byte) 32);
+        userService.saveUser("Sasha", "Tailor", (byte) 20);
+        userService.saveUser("Patric", "Malcovic", (byte) 33);
         System.out.println(userService.getAllUsers());
 
         userService.removeUserById(2);
